b19d1f8fa4872afa2896d1ee32b337370b34ce04
Rashad Gant <83968751+rashadgant@users.noreply.github.com>
2021-10-31 10:20:02 -0400

    fixed phonebook function

--- main.js
+++ main.js
@@ -1,8 +1,54 @@
-// ┌─────────────────┐
-// │ Your code here! │	
-// └─────────────────┘
+//1
+function isAdmin(user) {
+    if (user.userRole === 'ADMIN') {
+        return true;
+    } else {
+        return false;
+    }
+}
 
+//2
+function getEmail(user) {
+    const firstName = user.firstName.toLowerCase();
+    const lastName = user.lastName.toLowerCase();
+    const email = firstName + '.' + lastName + '@codeimmersives.com';
+    return email;
+}
 
+//3
+function getPlaylistLength(playlist) {
+    playlistLength = playlist.songs.length;
+    return playlistLength;
+}
+
+//4
+function getHardestHomework(homework) {
+  //Starting point for comparison
+  let lowestScore = 100;
+  //Output string
+  let hardest = '';
+  //Finds the lowest score
+  for (let i=0; i < homework.length; i++) {
+      let score = homework[i].averageScore;
+      if (score < lowestScore) {
+          lowestScore = score;
+          hardest = homework[i].name;
+      }
+  }
+  return hardest;
+}
+
+//5
+function createPhonebook(names, phoneNumbers) {
+    const phoneBook = {};
+    for (let i = 0; i < names.length; i++) {
+        let entryNames = names[i];
+        let entryNumbers = phoneNumbers[i];
+        let entry = entryNames.concat(':' + entryNumbers);
+        phoneBook[entryNames] = entryNumbers;
+
+    } return phoneBook;
+}
 
 // ┌─────────────────────────────────────┐
 // │ Do not modify code below this line. │
